6b905cab475862b4f9c714fa6e2099dae22704ef
hojeong1387 <hojeong1387@gmail.com>
2024-08-11 17:59:42 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Messenger msg="하이~"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Messenger from './components/Messenger.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Messenger
   }
 }
 </script>

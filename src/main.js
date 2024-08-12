70c5b95fee1216c9301546b261c6c832275cc5ec
hojeong1387 <hojeong1387@gmail.com>
2024-08-12 19:37:23 +0900

    포트번호와 main.js vue not define 에러 이슈

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-//createApp(App).mount('#app')
-Vue.config.productionTip = false;
+createApp(App).mount("#app");
+// Vue.config.productionTip = false;
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount("#app");

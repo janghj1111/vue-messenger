00d5a439a1986201f7158eb8e5d52e1feae0a307
hojeong1387 <hojeong1387@gmail.com>
2024-10-13 18:25:04 +0900

    소스변경

--- src/components/Messenger.vue
+++ src/components/Messenger.vue
@@ -20,67 +20,62 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue'
 import Stomp from 'webstomp-client'
 import SockJS from 'sockjs-client'
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      userName: "",
-      message: "",
-      recvList: []
-    }
-  },
-  created() {
-    // App.vue가 생성되면 소켓 연결을 시도합니다.
-    this.connect()
-  },
-  methods: {
-    sendMessage (e) {
-      if(e.keyCode === 13 && this.userName !== '' && this.message !== ''){
-        this.send()
-        this.message = ''
-      }
-    },    
-    send() {
-      console.log("Send message:" + this.message);
-      if (this.stompClient && this.stompClient.connected) {
-        const msg = { 
-          userName: this.userName,
-          content: this.message 
-        };
-        this.stompClient.send("/receive", JSON.stringify(msg), {});
-      }
-    },    
-    connect() {
-      const serverURL = "http://localhost:9092"
-      let socket = new SockJS(serverURL);
-      this.stompClient = Stomp.over(socket);
-      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
-      this.stompClient.connect(
-        {},
-        frame => {
-          // 소켓 연결 성공
-          this.connected = true;
-          console.log('소켓 연결 성공', frame);
-          // 서버의 메시지 전송 endpoint를 구독합니다.
-          // 이런형태를 pub sub 구조라고 합니다.
-          this.stompClient.subscribe("/send", res => {
-            console.log('구독으로 받은 메시지 입니다.', res.body);
+const userName = ref('')
+const message = ref('')
+const recvList = ref([])
 
-            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
-            this.recvList.push(JSON.parse(res.body))
-          });
-        },
-        error => {
-          // 소켓 연결 실패
-          console.log('소켓 연결 실패', error);
-          this.connected = false;
-        }
-      );        
-    }
+let stompClient = null
+let connected = ref(false)
+
+const sendMessage = (e) => {
+  if (e.keyCode === 13 && userName.value !== '' && message.value !== '') {
+    send()
+    message.value = ''
   }
 }
+
+const send = () => {
+  console.log("Send message:" + message.value)
+  if (stompClient && stompClient.connected) {
+    const msg = { 
+      userName: userName.value,
+      content: message.value 
+    }
+    stompClient.send("/receive", JSON.stringify(msg), {})
+  }
+}
+
+const connect = () => {
+  const serverURL = "http://localhost:9092"
+  let socket = new SockJS(serverURL)
+  stompClient = Stomp.over(socket)
+  console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+  
+  stompClient.connect(
+    {},
+    (frame) => {
+      connected.value = true
+      console.log('소켓 연결 성공', frame)
+
+      // 구독 설정
+      stompClient.subscribe("/send", (res) => {
+        console.log('구독으로 받은 메시지 입니다.', res.body)
+        recvList.value.push(JSON.parse(res.body))
+      })
+    },
+    (error) => {
+      console.log('소켓 연결 실패', error)
+      connected.value = false
+    }
+  )
+}
+
+onMounted(() => {
+  connect()
+})
 </script>
